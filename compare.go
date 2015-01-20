6c34e6bb6efbbcc4a8ddf73e36b110890c1565a6
anlhord <anlhord@gmail.com>
2015-01-20 10:46:32 +0100

    Fix the comment as suggested by lint.

--- compare.go
+++ compare.go
@@ -16,47 +16,47 @@ import (
 	"math/cmplx"
 )
 
-// compare.Uint8 compares two numbers.
+// Uint8 compares two numbers.
 func Uint8(a, b *uint8) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Uint16 compares two numbers.
+// Uint16 compares two numbers.
 func Uint16(a, b *uint16) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Int8 compares two numbers.
+// Int8 compares two numbers.
 func Int8(a, b *int8) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Int16 compares two numbers.
+// Int16 compares two numbers.
 func Int16(a, b *int16) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Int32 compares two numbers.
+// Int32 compares two numbers.
 func Int32(a, b *int32) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Int compares two numbers.
+// Int compares two numbers.
 func Int(a, b *int) (r int) {
 	return *a - *b
 }
 
-// compare.Byte compares two numbers.
+// Byte compares two numbers.
 func Byte(a, b *byte) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Rune compares two numbers.
+// Rune compares two numbers.
 func Rune(a, b *rune) int {
 	return int(*a) - int(*b)
 }
 
-// compare.Uint32 compares two numbers.
+// Uint32 compares two numbers.
 func Uint32(a, b *uint32) (r int) {
 	r = int(*a>>16) - int(*b>>16)
 	if r != 0 {
@@ -65,7 +65,7 @@ func Uint32(a, b *uint32) (r int) {
 	return int(*a) - int(*b)
 }
 
-// compare.Uint64 compares two numbers.
+// Uint64 compares two numbers.
 func Uint64(a, b *uint64) (r int) {
 	r = int(*a>>32) - int(*b>>32)
 	if r != 0 {
@@ -78,7 +78,7 @@ func Uint64(a, b *uint64) (r int) {
 	return int(*a) - int(*b)
 }
 
-// compare.Int64 compares two numbers.
+// Int64 compares two numbers.
 func Int64(a, b *int64) (r int) {
 	r = int(*a>>32) - int(*b>>32)
 	if r != 0 {
@@ -87,14 +87,14 @@ func Int64(a, b *int64) (r int) {
 	return int(*a) - int(*b)
 }
 
-// compare.Float32 compares two numbers.
+// Float32 compares two numbers.
 func Float32(a, b *float32) int {
 	r := *a - *b
 	rr := int32(math.Float32bits(r))
 	return int(rr)
 }
 
-// compare.Float64 compares two numbers.
+// Float64 compares two numbers.
 func Float64(a, b *float64) int {
 	p := *a - *b
 	q := int64(math.Float64bits(p))
@@ -105,7 +105,7 @@ func Float64(a, b *float64) int {
 	return int(r)
 }
 
-// compare.Uint compares two numbers.
+// Uint compares two numbers.
 func Uint(a, b *uint) (r int) {
 	r = int(*a>>32) - int(*b>>32)
 	if r != 0 {
@@ -118,7 +118,7 @@ func Uint(a, b *uint) (r int) {
 	return int(*a) - int(*b)
 }
 
-// compare.Abs compares two complex numbers by absolute value.
+// Abs compares two complex numbers by absolute value.
 func Abs(a, b *complex128) int {
 	fa := cmplx.Abs(*a)
 	fb := cmplx.Abs(*b)
@@ -131,7 +131,7 @@ func Abs(a, b *complex128) int {
 	return int(r)
 }
 
-// compare.Real compares two complex numbers by real part.
+// Real compares two complex numbers by real part.
 func Real(a, b *complex128) int {
 	fa := real(*a)
 	fb := real(*b)
@@ -144,7 +144,7 @@ func Real(a, b *complex128) int {
 	return int(r)
 }
 
-// compare.Imag compares two complex numbers by imaginary part.
+// Imag compares two complex numbers by imaginary part.
 func Imag(a, b *complex128) int {
 	fa := imag(*a)
 	fb := imag(*b)
